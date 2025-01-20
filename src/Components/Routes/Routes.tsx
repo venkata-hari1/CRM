adc3095248281f58a6d6bc4e4f3c13a02867e8b2
venkata-hari1 <venkat.hari@megaviztech.com>
2025-01-20 14:32:10 +0530

    new version

--- src/Components/Routes/Routes.tsx
+++ src/Components/Routes/Routes.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router';
+import { Routes, Route } from 'react-router-dom';
 import Login from '../Authentication/Login';
 import ForgetPassword from '../Authentication/ForgetPassword';
 import ResetPassword from '../Authentication/ResetPassword';
